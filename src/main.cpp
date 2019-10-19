54efbbd54a2e9d5b49dc18f833408841baad0bb6
Julian Toumey <jtoumey@gmail.com>
2019-10-18 23:42:31 -0400

    Clean up deprecated stuff.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-#include "grid/field.hpp"
+#include "framework/field.hpp"
 
 int main(void)
 {
@@ -13,11 +13,15 @@ int main(void)
     double xmax = 1.0;
 	double ymax = 1.0;
 
+    double dx = xmax/npx;
+    double dy = xmax/npy;
+
     // INITIAL FIELDS
     double init_pres = 101325;
     double init_dens = 1.0;
     double init_velocity_u = 1.5;
     double init_velocity_v = 2.0;
+    double gamma = 1.0;
 
     Field<double> pressure(npx, npy, init_pres);
     Field<double> vel_u(npx, npy, init_velocity_u);
@@ -25,16 +29,15 @@ int main(void)
 
     Field<double> density(npx, npy, init_dens);
 
-    Field<double> temp(30, 294);
-
-    double testtt = temp(7);
-    std::cout << "one-d temp " << testtt <<std::endl;
-    double test = temp(24, 0);
-
     // OUTER LOOP--SIMPLE PRESSURE-VELOCITY COUPLING (STEADY-STATE)
 
     // Loop over interior cells--momentum equation
     // _e.g._, calcMomentumFlux()
+
+    /* Convective fluxes */
+    double conv_flux_uw, conv_flux_ue, conv_flux_us, conv_flux_un;
+    /* Diffusive fluxes*/
+    double diff_flux_uw, diff_flux_ue, diff_flux_us, diff_flux_un;
 
     for (int ii = 1; ii < (npx - 1); ++ii)
     {
@@ -49,10 +52,18 @@ int main(void)
             double v_N = vel_v(ii, jj + 1);
 
             // Interpolate cell face flux via central difference
-            double F_w = 0.5*(u_W + u_P)*density(ii, jj);
-            double F_e = 0.5*(u_P + u_E)*density(ii, jj);
-            double F_s = 0.5*(v_P + v_S)*density(ii, jj);
-            double F_n = 0.5*(v_P + v_N)*density(ii, jj);
+            conv_flux_uw = 0.5*(u_W + u_P)*density(ii - 1, jj);
+            conv_flux_ue = 0.5*(u_P + u_E)*density(ii + 1, jj);
+            conv_flux_us = 0.5*(v_P + v_S)*density(ii, jj - 1);
+            conv_flux_un = 0.5*(v_P + v_N)*density(ii, jj + 1);
+
+            std::cout << conv_flux_ue << std::endl;
+
+            diff_flux_uw = gamma/dx;
+            diff_flux_ue = gamma/dx;
+            diff_flux_us = gamma/dy;
+            diff_flux_un = gamma/dy;
+
         }
     }
 
