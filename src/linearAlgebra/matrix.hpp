8b6936ef0fa09769a80e57968a7506c3daba6031
Julian Toumey <jtoumey@gmail.com>
2020-01-26 13:30:31 -0500

    Hashing out a linear algebra class.

---
+++ src/linearAlgebra/matrix.hpp
@@ -0,0 +1,23 @@
+// Julian M. Toumey
+// Spring 2020
+// ME5311: Computational Fluid Dynamics
+
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+namespace LinearAlgebra
+{
+
+class Matrix
+{
+private:
+
+public:
+
+};
+
+}
+
+
+#endif // MATRIX_H
